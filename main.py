f273623b9ae7b929b3126285a806a1fdbc3fe381
Unchained112 <51079412+Unchained112@users.noreply.github.com>
2023-05-26 16:04:25 +0200

    add basic weapon aiming control

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
+import math
 import arcade
 from pyglet.math import Vec2
 
-''' GLOBAL VARIABLE '''
+""" GLOBAL VARIABLE """
 # Color palette
 GROUND_WHITE = (240, 237, 212)
 BLACK = (0, 0, 0)
@@ -20,73 +21,122 @@ SCREEN_HEIGHT = int(720)
 CAMERA_SPEED = 0.6
 
 # Animations
-BODY_ANIM = [-1,-1,-1,-1,-1,-1,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              1, 1, 1, 1, 1, 1,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-BODY_WALK = [ 1, 1, 1,-1,-1,-1, 1, 1, 1,-1,-1,-1]
-L_WALK_X  = [-1,-1,-1, 1, 1, 1, 1, 1, 1,-1,-1,-1]
-L_WALK_Y  = [ 1, 1, 1,-1,-1,-1, 1, 1, 1,-1,-1,-1]
-R_WALK_X  = [ 1, 1, 1,-1,-1,-1,-1,-1,-1, 1, 1, 1]
-R_WALK_Y  = [ 1, 1, 1,-1,-1,-1, 1, 1, 1,-1,-1,-1]
+BODY_ANIM = [
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+]
+BODY_WALK = [1, 1, 1, -1, -1, -1, 1, 1, 1, -1, -1, -1]
+L_WALK_X = [-1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1]
+L_WALK_Y = [1, 1, 1, -1, -1, -1, 1, 1, 1, -1, -1, -1]
+R_WALK_X = [1, 1, 1, -1, -1, -1, -1, -1, -1, 1, 1, 1]
+R_WALK_Y = [1, 1, 1, -1, -1, -1, 1, 1, 1, -1, -1, -1]
+
+
+def get_sin(v: Vec2) -> float:
+    """Get sine value of a given vector."""
+
+    return v.y / v.distance(Vec2(0, 0))
 
 class GameObject:
-    ''' GameObject Base Class. '''
+    """GameObject Base Class."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.pos = Vec2(0, 0)
 
     # update the GameObject logic
-    def update(self):
+    def update(self) -> None:
         pass
 
     # render the GameObject
-    def draw(self):
+    def draw(self) -> None:
         pass
 
-class FixedWall(GameObject):
-    ''' Fixed wall for background in the room. '''
 
-    def __init__(self, x = 0, y = 0):
+class FixedWall(GameObject):
+    """Fixed wall for background in the room."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
         self.pos = Vec2(x, y)
         self.grid_idx = (0, 0)
         self.len = 30
 
         # Wall
-        self.sprite = arcade.Sprite(filename = "./graphics/FixedWall1.png",
-                                    center_x = self.pos.x,
-                                    center_y = self.pos.y,
-                                    image_width = 30,
-                                    image_height = 30)
+        self.sprite = arcade.Sprite(
+            filename="./graphics/FixedWall1.png",
+            center_x=self.pos.x,
+            center_y=self.pos.y,
+            image_width=30,
+            image_height=30,
+        )
 
-    def draw(self):
+    def draw(self) -> None:
         self.sprite.draw()
 
-class Room:
-    ''' Room as a background in the game scene. '''
 
-    def __init__(self, width = 1500, height = 900):
+class Room:
+    """Room as a background in the game scene."""
+
+    def __init__(self, width: float = 1500, height: float = 900) -> None:
+        """width and heigh should be mutiples of 30 (Wall size)."""
         self.width = width
         self.height = height
         self.pos = Vec2(self.width / 2, self.height / 2)
-        self.walls = [FixedWall(50, 50),
-                      FixedWall(50, 100),
-                      FixedWall(100, 100)]
-        for i in range(0, 50):
+        self.walls = [FixedWall(50, 50), FixedWall(50, 100), FixedWall(100, 100)]
+        self.grid_w = int(self.width / 30)
+        self.grid_h = int(self.height / 30)
+
+        # Set boundary walls
+        for i in range(0, self.grid_w):
             self.walls.append(FixedWall(15 + i * 30, 15))
-        
-    def draw(self):
+            self.walls.append(FixedWall(15 + i * 30, self.height - 15))
+        for i in range(1, self.grid_h - 1):
+            self.walls.append(FixedWall(15, 15 + i * 30))
+            self.walls.append(FixedWall(self.width - 15, 15 + i * 30))
+
+    def draw(self) -> None:
         # Room background
-        arcade.draw_rectangle_filled(self.pos.x, self.pos.y,
-                                     self.width, self.height, GROUND_WHITE)
+        arcade.draw_rectangle_filled(
+            self.pos.x, self.pos.y, self.width, self.height, GROUND_WHITE
+        )
         # Walls
         for wall in self.walls:
             wall.draw()
 
-class Character(GameObject):
-    ''' Character base class. '''
 
-    def __init__(self, x = 0, y = 0):
+class Character(GameObject):
+    """Character base class."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
         # Perporties
         self.is_walking = False
         self.speed = 3
@@ -95,7 +145,7 @@ class Character(GameObject):
         self.pos = Vec2(x, y)
 
         # Relative positions for visuals
-        self.body_pos = Vec2(0, 0) # controls the actual movement
+        self.body_pos = Vec2(0, 0)  # controls the actual movement
         self.foot_l_pos = Vec2(-8, -16)
         self.foot_r_pos = Vec2(8, -16)
         self.collider_pos = Vec2(0, -3)
@@ -118,25 +168,28 @@ class Character(GameObject):
         # Body sprite
         self.body = arcade.Sprite()
         # Feet sprite
-        self.foot_l = arcade.Sprite(filename = "./graphics/Foot.png",
-                                    center_x = self.foot_l_pos.x + self.pos.x,
-                                    center_y = self.foot_l_pos.y + self.pos.x,
-                                    image_width = 4,
-                                    image_height = 4,
-                                    scale = 1)
-        self.foot_r = arcade.Sprite(filename = "./graphics/Foot.png",
-                                    center_x = self.foot_r_pos.x + self.pos.x,
-                                    center_y = self.foot_r_pos.y + self.pos.x,
-                                    image_width = 4,
-                                    image_height = 4,
-                                    scale = 1)
+        self.foot_l = arcade.Sprite(
+            filename="./graphics/Foot.png",
+            center_x=self.foot_l_pos.x + self.pos.x,
+            center_y=self.foot_l_pos.y + self.pos.x,
+            image_width=4,
+            image_height=4,
+            scale=1,
+        )
+        self.foot_r = arcade.Sprite(
+            filename="./graphics/Foot.png",
+            center_x=self.foot_r_pos.x + self.pos.x,
+            center_y=self.foot_r_pos.y + self.pos.x,
+            image_width=4,
+            image_height=4,
+            scale=1,
+        )
         # Collider sprite for physics
         self.box_collider = arcade.SpriteSolidColor(20, 30, DARK_GRAY)
         self.box_collider.center_x = self.pos.x + self.collider_pos.x
         self.box_collider.center_y = self.pos.y + self.collider_pos.y
 
-    def move(self):
-
+    def move(self) -> None:
         if self.box_collider.change_x != 0 or self.box_collider.change_y != 0:
             self.is_walking = True
         else:
@@ -153,11 +206,11 @@ class Character(GameObject):
         self.foot_r.center_x = self.pos.x + self.foot_r_pos.x
         self.foot_r.center_y = self.pos.y + self.foot_r_pos.y
 
-    def update(self):
+    def update(self) -> None:
         self.move()
 
         # Body animation
-        if self.body_move_frames == 0: # reset frames
+        if self.body_move_frames == 0:  # reset frames
             self.body_move_frames = self.body_move_frames_max
             self.body_move_up = not self.body_move_up
 
@@ -165,7 +218,7 @@ class Character(GameObject):
         self.body.center_y += BODY_ANIM[self.body_move_frames]
 
         # Feet animation
-        if self.walking_frames == 0: # reset frames
+        if self.walking_frames == 0:  # reset frames
             self.walking_frames = self.walking_frames_max
 
         self.walking_frames -= 1
@@ -183,27 +236,36 @@ class Character(GameObject):
             self.foot_r.center_y = self.foot_r_pos.y + self.pos.y
             self.walking_frames = self.walking_frames_max
 
-    def draw(self):
+    def draw(self) -> None:
         # self.box_collider.draw()
         self.body.draw()
         self.foot_l.draw()
         self.foot_r.draw()
 
-class Player(Character):
-    ''' Player game object. '''
 
-    def __init__(self, x, y):
+class Player(Character):
+    """Player game object."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
         super().__init__(x, y)
 
         # Player body sprite
-        self.body = arcade.Sprite(filename = "./graphics/Player.png",
-                                    center_x = self.body_pos.x + self.pos.x,
-                                    center_y = self.body_pos.y + self.pos.x,
-                                    image_width = 20,
-                                    image_height = 24,
-                                    scale = 1)
+        self.body = arcade.Sprite(
+            filename="./graphics/Player.png",
+            center_x=self.body_pos.x + self.pos.x,
+            center_y=self.body_pos.y + self.pos.x,
+            image_width=20,
+            image_height=24,
+            scale=1,
+        )
 
-    def move(self):
+        # Weapon
+        self.weapon_pos = Vec2(16, -2)
+        self.weapons = []
+        self.weapon_index = 0
+        self.add_weapon()
+
+    def move(self) -> None:
         self.box_collider.change_x = 0
         self.box_collider.change_y = 0
 
@@ -218,40 +280,126 @@ class Player(Character):
 
         super().move()
 
-class EnemyWhite(Character):
-    ''' EnemyWhite class. '''
+        self.weapons[self.weapon_index].pos = self.pos + self.weapon_pos
 
-    def __init__(self, x = 0, y = 0):
+    def draw(self) -> None:
+        if self.weapons[self.weapon_index].is_right:
+            self.weapons[self.weapon_index].draw()
+            super().draw()
+        else:
+            super().draw()
+            self.weapons[self.weapon_index].draw()
+
+    def update(self) -> None:
+        super().update()
+        if self.weapons[self.weapon_index].is_right:
+            self.weapon_pos = Vec2(16, -2)
+        else:
+            self.weapon_pos = Vec2(9, -2)
+        self.weapons[self.weapon_index].update()
+
+    def add_weapon(self, name: str = "./graphics/Pistol.png") -> None:
+        gun = Gun(
+            name, x=self.pos.x + self.weapon_pos.x, y=self.pos.y + self.weapon_pos.y
+        )
+        self.weapons.append(gun)
+
+    def aim(self, mouse_pos: Vec2) -> None:
+        aim_pos = mouse_pos - self.pos
+        self.weapons[self.weapon_index].aim(aim_pos)
+
+
+class EnemyWhite(Character):
+    """EnemyWhite class."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
         super().__init__(x, y)
+        self.speed = 2
 
         # EnemyWhite body sprite
-        self.body = arcade.Sprite(filename = "./graphics/EnemyWhite.png",
-                                    center_x = self.body_pos.x + self.pos.x,
-                                    center_y = self.body_pos.y + self.pos.x,
-                                    image_width = 20,
-                                    image_height = 24,
-                                    scale = 1)
+        self.body = arcade.Sprite(
+            filename="./graphics/EnemyWhite.png",
+            center_x=self.body_pos.x + self.pos.x,
+            center_y=self.body_pos.y + self.pos.x,
+            image_width=20,
+            image_height=24,
+            scale=1,
+        )
+
 
 class EnemyRed(Character):
-    ''' EnemyRed class. '''
+    """EnemyRed class."""
 
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, x: float = 0, y: float = 0) -> None:
         super().__init__(x, y)
+        self.speed = 2
 
         # EnemyRed body sprite
-        self.body = arcade.Sprite(filename = "./graphics/EnemyRed.png",
-                                    center_x = self.body_pos.x + self.pos.x,
-                                    center_y = self.body_pos.y + self.pos.x,
-                                    image_width = 20,
-                                    image_height = 26,
-                                    scale = 1)
+        self.body = arcade.Sprite(
+            filename="./graphics/EnemyRed.png",
+            center_x=self.body_pos.x + self.pos.x,
+            center_y=self.body_pos.y + self.pos.x,
+            image_width=20,
+            image_height=26,
+            scale=1,
+        )
+
+
+class Gun(GameObject):
+    """Gun waepon class."""
+
+    def __init__(
+        self, gun_name: str = "./graphics/Pistol.png", x: float = 0, y: float = 0
+    ) -> None:
+        self.damage = 0
+        self.cd = 0.5
+        self.pos = Vec2(x, y)
+        self.is_right = True
+        self.textures = [
+            arcade.load_texture(gun_name),
+            arcade.load_texture(gun_name, flipped_horizontally=True),
+        ]
+        self.sprite = arcade.Sprite(
+            filename=gun_name,
+            center_x=self.pos.x,
+            center_y=self.pos.y,
+            image_width=20,
+            image_height=10,
+        )
+        # self.sprite.angle = 90
+
+    def draw(self) -> None:
+        self.sprite.draw()
+
+    def update(self) -> None:
+        self.sprite.center_x = self.pos.x
+        self.sprite.center_y = self.pos.y
+        if self.is_right:
+            self.sprite.texture = self.textures[0]
+        else:
+            self.sprite.texture = self.textures[1]
+
+    def aim(self, aim_pos: Vec2) -> None:
+        if aim_pos.x >= 0:
+            self.is_right = True
+            rotate_angle = math.degrees(math.asin(get_sin(aim_pos)))
+        else:
+            self.is_right = False
+            rotate_angle = -math.degrees(math.asin(get_sin(aim_pos)))
+
+        self.sprite.angle = rotate_angle
+
 
 class BoxHead(arcade.Window):
-    """ Main application class. """
+    """Main application class."""
 
     def __init__(self, width, height, title):
         """Initializer"""
         super().__init__(width, height, title, resizable=True)
+        self.set_mouse_visible(False)
+        self.mouse_x = None
+        self.mouse_y = None
+        self.mouse_pos = Vec2(0, 0)
 
         # Sprite lists
         self.wall_list = None
@@ -277,7 +425,7 @@ class BoxHead(arcade.Window):
         self.camera_gui = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
+        """Set up the game and initialize the variables."""
 
         # Sprite lists
         self.wall_list = arcade.SpriteList()
@@ -291,7 +439,9 @@ class BoxHead(arcade.Window):
         # set up the player
         self.player = Player(float(SCREEN_WIDTH / 2), float(SCREEN_HEIGHT / 2))
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player.box_collider, self.wall_list)
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player.box_collider, self.wall_list
+        )
 
         self.enemy_test_1 = EnemyRed(200, 300)
         self.enemy_test_2 = EnemyWhite(200, 400)
@@ -300,7 +450,7 @@ class BoxHead(arcade.Window):
         arcade.set_background_color(BLACK)
 
     def on_draw(self):
-        """ Render the screen. """
+        """Render the screen."""
 
         # This command has to happen before we start drawing
         self.clear()
@@ -308,7 +458,7 @@ class BoxHead(arcade.Window):
         # Select the camera we'll use to draw all our sprites
         self.camera_sprites.use()
 
-        # Draw all the sprites.
+        # draw all the sprites.
         # self.wall_list.draw()
         self.game_room.draw()
         self.player.draw()
@@ -316,9 +466,15 @@ class BoxHead(arcade.Window):
         self.enemy_test_1.draw()
         self.enemy_test_2.draw()
 
-
         # Select the (unscrolled) camera for our GUI
-        # self.camera_gui.use()
+        self.camera_gui.use()
+
+        # Mouse cursor
+        if self.mouse_x and self.mouse_y:
+            # TODO: change mouse cursor image
+            arcade.draw_rectangle_filled(
+                self.mouse_x, self.mouse_y, 4, 4, arcade.color.RED
+            )
 
         # Render the GUI
         # arcade.draw_rectangle_filled(self.width // 2,
@@ -329,30 +485,6 @@ class BoxHead(arcade.Window):
         # text = f"Scroll value: ({self.camera_sprites.position[0]:5.1f}, " \
         #        f"{self.camera_sprites.position[1]:5.1f})"
         # arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
-
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
-
-        if key == arcade.key.W:
-            self.player.move_up = True
-        elif key == arcade.key.S:
-            self.player.move_down = True
-        elif key == arcade.key.A:
-            self.player.move_left = True
-        elif key == arcade.key.D:
-            self.player.move_right = True
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
-        if key == arcade.key.W:
-            self.player.move_up = False
-        elif key == arcade.key.S:
-            self.player.move_down = False
-        elif key == arcade.key.A:
-            self.player.move_left = False
-        elif key == arcade.key.D:
-            self.player.move_right = False
 
     def on_update(self, delta_time):
         self.player.update()
@@ -366,6 +498,37 @@ class BoxHead(arcade.Window):
         # example though.)
         self.physics_engine.update()
 
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.W:
+            self.player.move_up = True
+        elif key == arcade.key.S:
+            self.player.move_down = True
+        elif key == arcade.key.A:
+            self.player.move_left = True
+        elif key == arcade.key.D:
+            self.player.move_right = True
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+
+        if key == arcade.key.W:
+            self.player.move_up = False
+        elif key == arcade.key.S:
+            self.player.move_down = False
+        elif key == arcade.key.A:
+            self.player.move_left = False
+        elif key == arcade.key.D:
+            self.player.move_right = False
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_x = x
+        self.mouse_y = y
+        self.mouse_pos.x = self.mouse_x + self.camera_sprites.position.x
+        self.mouse_pos.y = self.mouse_y + self.camera_sprites.position.y
+        self.player.aim(self.mouse_pos)
+
     def scroll_to_player(self):
         """
         Scroll the window to the player.
@@ -377,17 +540,23 @@ class BoxHead(arcade.Window):
         """
         position = Vec2(self.camera_sprites.position.x, self.camera_sprites.position.y)
         # limit the camera position within the room
-        if self.player.pos.x > SCREEN_WIDTH / 2 - 15 \
-            and self.game_room.width - self.player.pos.x > SCREEN_WIDTH / 2:
+        if (
+            self.player.pos.x > float(SCREEN_WIDTH / 2)
+            and self.game_room.width - self.player.pos.x > float(SCREEN_WIDTH / 2)
+        ):
             position.x = self.player.pos.x - SCREEN_WIDTH / 2
-        if self.player.pos.y > SCREEN_HEIGHT / 2 - 15 \
-            and self.game_room.height - self.player.pos.y > SCREEN_HEIGHT / 2:
+        if (
+            self.player.pos.y > float(SCREEN_HEIGHT / 2)
+            and self.game_room.height - self.player.pos.y > float(SCREEN_HEIGHT / 2)
+        ):
             position.y = self.player.pos.y - SCREEN_HEIGHT / 2
 
         self.camera_sprites.move_to(position, CAMERA_SPEED)
 
+
 def main():
-    """ Main function """
+    """Main function"""
+
     window = BoxHead(SCREEN_WIDTH, SCREEN_HEIGHT, "BoxHead 2D")
     window.setup()
     arcade.run()
