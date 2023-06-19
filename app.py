a33f7a09fbe8fffa0dbebfb72a7ac630ddb48ad7
Unchained112 <51079412+Unchained112@users.noreply.github.com>
2023-06-19 21:11:18 +0200

    refactor init commit

---
+++ app.py
@@ -0,0 +1,108 @@
+"""
+Use sprites to scroll around a large screen.
+
+Simple program to show basic sprite usage.
+
+Artwork from https://kenney.nl
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.full_screen_example
+"""
+
+import arcade
+import os
+
+SPRITE_SCALING = 0.5
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Full Screen Example"
+
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+VIEWPORT_MARGIN = 40
+
+MOVEMENT_SPEED = 5
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self):
+        """
+        Initializer
+        """
+        # Open a window in full screen mode. Remove fullscreen=True if
+        # you don't want to start this way.
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=True)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # This will get the size of the window, and set the viewport to match.
+        # So if the window is 1000x1000, then so will our viewport. If
+        # you want something different, then use those coordinates instead.
+        width, height = self.get_size()
+        self.set_viewport(0, width, 0, height)
+        arcade.set_background_color(arcade.color.AMAZON)
+        self.example_image = arcade.load_texture(":resources:images/tiles/boxCrate_double.png")
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        self.clear()
+
+        # Get viewport dimensions
+        left, screen_width, bottom, screen_height = self.get_viewport()
+
+        text_size = 18
+        # Draw text on the screen so the user has an idea of what is happening
+        arcade.draw_text("Press F to toggle between full screen and windowed mode, unstretched.",
+                         screen_width // 2, screen_height // 2 - 20,
+                         arcade.color.WHITE, text_size, anchor_x="center")
+        arcade.draw_text("Press S to toggle between full screen and windowed mode, stretched.",
+                         screen_width // 2, screen_height // 2 + 20,
+                         arcade.color.WHITE, text_size, anchor_x="center")
+
+        # Draw some boxes on the bottom so we can see how they change
+        for x in range(64, 800, 128):
+            y = 64
+            width = 128
+            height = 128
+            arcade.draw_texture_rectangle(x, y, width, height, self.example_image)
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        if key == arcade.key.F:
+            # User hits f. Flip between full and not full screen.
+            self.set_fullscreen(not self.fullscreen)
+
+            # Get the window coordinates. Match viewport to window coordinates
+            # so there is a one-to-one mapping.
+            width, height = self.get_size()
+            self.set_viewport(0, width, 0, height)
+
+        if key == arcade.key.S:
+            # User hits s. Flip between full and not full screen.
+            self.set_fullscreen(not self.fullscreen)
+
+            # Instead of a one-to-one mapping, stretch/squash window to match the
+            # constants. This does NOT respect aspect ratio. You'd need to
+            # do a bit of math for that.
+            self.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
+
+
+def main():
+    """ Main function """
+    MyGame()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ room.py
@@ -0,0 +1,112 @@
+import arcade
+import math
+from pyglet.math import Vec2
+from color import *
+
+WALL_SIZE = 30
+HALF_WALL_SIZE = 15
+
+class Wall(arcade.Sprite):
+    """Basic wall."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
+        self.pos = Vec2(x, y)
+        self.grid_idx = (math.floor(x / 30),
+                         math.floor(y / 30))
+
+        super().__init__(
+            filename=None,
+            center_x=self.pos.x,
+            center_y=self.pos.y,
+            image_width=WALL_SIZE,
+            image_height=WALL_SIZE,
+        )
+
+class WallCorner(Wall):
+    """Wall at the corner."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
+        super().__init__(x, y)
+        self.texture = arcade.load_texture("graphics/WallCorner.png")
+        self.shadow = arcade.Sprite(
+            center_x=self.pos.x - 3,
+            center_y=self.pos.y - 3,
+            scale=1,
+        )
+        self.shadow.texture = arcade.make_soft_square_texture(
+            30, LIGHT_BLACK, 200, 100)
+
+    def draw(self) -> None:
+        self.shadow.draw()
+        super().draw()
+
+
+class WallSideHorizontal(Wall):
+    """Wall along the horizontal side."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
+        super().__init__(x, y)
+        self.texture = arcade.load_texture("graphics/WallSide.png")
+
+
+class WallSideVertical(Wall):
+    """Wall along the vertical side."""
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
+        super().__init__(x, y)
+        self.texture = arcade.load_texture("graphics/WallSide.png")
+        self.angle = -90
+
+class Room:
+    """Room base class."""
+
+    def __init__(self, width: float = 2100, height: float = 1200) -> None:
+        """width and height should be multiple of the wall size"""
+        self.width = width
+        self.height = height
+        self.pos = Vec2(self.width / 2, self.height / 2)
+
+        self.grid_w = int(self.width / WALL_SIZE)
+        self.grid_h = int(self.height / WALL_SIZE)
+        self.grid = {(i, j): 0 for i in range(self.grid_w)
+                     for j in range(self.grid_h)}
+
+        self.walls = []
+
+    def draw_ground(self) -> None:
+        pass
+
+    def draw_walls(self) -> None:
+        pass
+
+class StartRoom(Room):
+    """Room for start menu."""
+
+    def __init__(self, width: float = 2100, height: float = 1200) -> None:
+        super().__init__(width, height)
+        # set boundary corner walls
+        self.walls = [WallCorner(HALF_WALL_SIZE, HALF_WALL_SIZE), WallCorner(HALF_WALL_SIZE, self.height - HALF_WALL_SIZE), WallCorner(
+            self.width - HALF_WALL_SIZE, HALF_WALL_SIZE), WallCorner(self.width - HALF_WALL_SIZE, self.height - HALF_WALL_SIZE)]
+
+        # set bottom and top walls
+        for i in range(1, self.grid_w - 1):
+            self.walls.append(WallSideHorizontal(
+                HALF_WALL_SIZE + i * WALL_SIZE, HALF_WALL_SIZE))
+            self.walls.append(WallSideHorizontal(
+                HALF_WALL_SIZE + i * WALL_SIZE, self.height - HALF_WALL_SIZE))
+
+        # set left and right walls
+        for i in range(1, self.grid_h - 1):
+            self.walls.append(WallSideVertical(
+                HALF_WALL_SIZE, HALF_WALL_SIZE + i * WALL_SIZE))
+            self.walls.append(WallSideVertical(
+                self.width - HALF_WALL_SIZE, HALF_WALL_SIZE + i * WALL_SIZE))
+
+    def draw_ground(self) -> None:
+        arcade.draw_rectangle_filled(
+            self.pos.x, self.pos.y, self.width, self.height, GROUND_WHITE
+        )
+
+    def draw_walls(self) -> None:
+        for wall in self.walls:
+            wall.draw()
